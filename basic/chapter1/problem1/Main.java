6850de5253c7c0358b9c6631b6ff1e29376f3991
sghwan <anan123456@naver.com>
2022-08-08 23:34:00 +0900

    first commit test
    first commit test

---
+++ basic/chapter1/problem1/Main.java
@@ -0,0 +1,4 @@
+package chapter1.problem1;
+
+public class Main {
+}
---
+++ basic/chapter1/problem10/Main.java
@@ -0,0 +1,54 @@
+package chapter1.problem2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static int[] solution(String s, char ch) {
+        char[] arr = s.toCharArray();
+        int[] answer = new int[s.length()];
+        int p = 1000;
+
+        for(int i = 0; i < s.length(); i++) {
+            if(arr[i] == ch) {
+                p = 0;
+                answer[i] = p;
+            } else {
+                p += 1;
+                answer[i] = p;
+            }
+        }
+
+        for(int i = s.length() - 1; i >= 0; i--) {
+            if(arr[i] == ch) {
+                p = 0;
+                answer[i] = p;
+            } else {
+                p += 1;
+                if(answer[i] > p)
+                    answer[i] = p;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String s = st.nextToken();
+        char ch = st.nextToken().charAt(0);
+
+        int[] answer = solution(s, ch);
+
+        for(int a : answer) {
+            System.out.print(a+ " ");
+        }
+
+        return;
+    }
+}
