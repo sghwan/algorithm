296e73aec75cf479fc83df2983774116c832b6e2
sghwan <anan123456@naver.com>
2022-10-04 16:24:54 +0900

    다음큰 숫자

---
+++ lv2/pb7/Main.java
@@ -0,0 +1,31 @@
+package lv2.pb7;
+
+class Solution {
+
+    public int countOne(int n) {
+        int cnt = 0;
+        String binary = Integer.toString(n, 2);
+        for (char x : binary.toCharArray()) {
+            if(x == '1') cnt++;
+        }
+        return cnt;
+    }
+
+    public int solution(int n) {
+        int answer = 0;
+        int cnt = countOne(n);
+        int s = n + 1;
+        boolean flag = false;
+
+        while(!flag) {
+            int nextCnt = countOne(s);
+            if(nextCnt == cnt) flag = true;
+            else s++;
+        }
+
+        answer = s;
+
+        return answer;
+    }
+
+}
