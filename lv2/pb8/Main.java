2028afa81b83aa6dd11744550b19d7fa68506781
sghwan <anan123456@naver.com>
2022-10-04 16:25:05 +0900

    카펫

---
+++ lv2/pb8/Main.java
@@ -0,0 +1,21 @@
+package lv2.pb8;
+
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = {0, 0};
+
+        for (int height = 1; height * height <= yellow; height++) {
+            if(yellow % height == 0) {
+                int width = yellow / height;
+                int requiredBrown = 2 * height + 2 * width + 4;
+                if(requiredBrown == brown) {
+                    answer[0] = width + 2;
+                    answer[1] = height + 2;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
