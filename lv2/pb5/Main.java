621fc5712d00544c843009638d1a089b175a6d39
sghwan <anan123456@naver.com>
2022-10-04 16:24:37 +0900

    숫자의 표현

---
+++ lv2/pb5/Main.java
@@ -0,0 +1,19 @@
+package lv2.pb5;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int sum = 0, lt = 1;
+
+        for(int i = 1; i <= n / 2 + 1; i++) {
+            sum += i;
+            if(sum == n) answer++;
+            while(sum >= n) {
+                sum -= lt++;
+                if(sum == n) answer++;
+            }
+        }
+        return answer;
+    }
+}
+
