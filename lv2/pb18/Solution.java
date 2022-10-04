2b72a293158a2a10b1bcbe807aaf46649c2b702e
sghwan <anan123456@naver.com>
2022-10-04 21:32:50 +0900

    괄호 회전하기

---
+++ lv2/pb18/Solution.java
@@ -0,0 +1,32 @@
+package lv2.pb18;
+
+import java.util.Stack;
+
+class Solution {
+    public int checkBracket(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char x : s.toCharArray()) {
+            if (x == '[' || x == '{' || x == '(') stack.push(x);
+            else {
+                if (stack.isEmpty()) return 1;
+                if (stack.peek() == '[' && x == ']') stack.pop();
+                else if (stack.peek() == '{' && x == '}') stack.pop();
+                else if (stack.peek() == '(' && x == ')') stack.pop();
+            }
+        }
+
+        return stack.size();
+    }
+
+    public int solution(String s) {
+        int answer = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            s = s.substring(1) + s.charAt(0);
+            if (checkBracket(s) == 0) answer++;
+        }
+
+        return answer;
+    }
+}
