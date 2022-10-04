168c06aef148b4767426ed5b91ba2c14e294a69e
sghwan <anan123456@naver.com>
2022-10-04 16:23:53 +0900

    JadenCase 문자열 만들기

---
+++ lv2/pb2/Main.java
@@ -0,0 +1,27 @@
+package lv2.pb2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Solution {
+    public String solution(String s) {
+        boolean isNextUpper = true;
+        StringBuffer sb = new StringBuffer();
+
+        for ( int inx=0; inx<s.length(); inx++ ) {
+            char c = s.charAt(inx);
+            if ( inx == 0 || isNextUpper ) {
+                sb.append(Character.toUpperCase(c));
+                isNextUpper = false;
+            } else {
+                sb.append(Character.toLowerCase(c));
+            }
+
+            if ( c == ' ') {
+                isNextUpper = true;
+            }
+        }
+        return sb.toString();
+    }
+}
