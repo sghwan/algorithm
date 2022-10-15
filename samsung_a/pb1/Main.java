3409b7ceb9c7ae77101920c0615db7f454b51ebd
sghwan <anan123456@naver.com>
2022-10-15 19:33:53 +0900

    삼성 sw 역량테스트 a형: 파이프 문제

---
+++ samsung_a/pb1/Main.java
@@ -0,0 +1,52 @@
+package samsung_a.pb1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[][] home;
+    static int n;
+    static int count = 0;
+
+    public static void dfs(int x, int y, int degree) {
+        if (x == n - 1 && y == n - 1) {
+            count++;
+            return;
+        }
+        if (degree == 0) {
+            if (y + 1 < n && home[x][y + 1] == 0)
+                dfs(x, y + 1, 0);
+        } else if (degree == 90) {
+            if (x + 1 < n && home[x + 1][y] == 0)
+                dfs(x + 1, y, 90);
+        } else {
+            if (y + 1 < n && home[x][y + 1] == 0)
+                dfs(x, y + 1, 0);
+            if (x + 1 < n && home[x + 1][y] == 0)
+                dfs(x + 1, y, 90);
+        }
+        if (x + 1 < n && y + 1 < n && home[x + 1][y] == 0 && home[x][y + 1] == 0 && home[x + 1][y + 1] == 0)
+            dfs(x + 1, y + 1, 45);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        home = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                home[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        dfs(0, 1, 0);
+
+        System.out.println(count);
+    }
+}
+
